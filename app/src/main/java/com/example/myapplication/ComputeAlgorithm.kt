0fd258c5c84e7c2605c2c0958e77c2227786a95e
vishwajithkv <vishwajithkv@users.noreply.github.com>
2026-03-06 22:20:02 +0530

    Implement LRU and Optimal Replacement Algorithms

--- app/src/main/java/com/example/myapplication/ComputeAlgorithm.kt
+++ app/src/main/java/com/example/myapplication/ComputeAlgorithm.kt
@@ -43,3 +43,106 @@ fun firstInFirstOutAlgorithm(list: List<String>, frameSize: Int): List<Step> {
     }
     return steps
 }
+
+fun leastRecentlyUsedAlgorithm(list: List<String>, frameSize: Int): List<Step> {
+    val steps = ArrayList<Step>()
+    val frames = mutableListOf<String>()
+    val linked = LinkedHashMap<String, String>(frameSize, 0.75F, true)
+    for (item in list) {
+        if (linked.contains(item)) {
+            linked[item]
+            steps.add(
+                Step(
+                    isHit = true,
+                    currentFrame = frames.toList(),
+                    evictedItem = null
+                )
+            )
+        } else if (linked.size != frameSize) {
+            linked[item] = item
+            frames.add(item)
+            steps.add(
+                Step(
+                    isHit = false,
+                    currentFrame = frames.toList(),
+                    evictedItem = null
+                )
+            )
+        } else {
+            val lruKey = linked.keys.first()
+            linked.remove(lruKey)
+            linked[item] = item
+
+            val index = frames.indexOf(lruKey)
+            frames[index] = item
+
+            steps.add(
+                Step(
+                    isHit = false,
+                    currentFrame = frames.toList(),
+                    evictedItem = lruKey
+                )
+            )
+        }
+    }
+    return steps
+}
+
+fun optimalAlgorithm(list: List<String>, frameSize: Int): List<Step> {
+    val steps = ArrayList<Step>()
+    val frames = mutableListOf<String>()
+
+
+    for (i in list.indices) {
+        val item = list[i]
+
+        if (frames.contains(item)) {
+            steps.add(
+                Step(
+                    isHit = true,
+                    currentFrame = frames.toList(),
+                    evictedItem = null
+                )
+            )
+        } else if (frames.size != frameSize) {
+            frames.add(item)
+            steps.add(
+                Step(
+                    isHit = false,
+                    currentFrame = frames.toList(),
+                    evictedItem = null
+                )
+            )
+        } else {
+            var victim = frames[0]
+            var farthestIndex = -1
+
+            for (page in frames) {
+                val nextUse = list.subList(i + 1, list.size).indexOf(page)
+
+                if (nextUse == -1) {
+                    victim = page
+                    break
+                }
+
+                if (nextUse > farthestIndex) {
+                    farthestIndex = nextUse
+                    victim = page
+                }
+            }
+
+            val index = frames.indexOf(victim)
+            frames[index] = item
+
+            steps.add(
+                Step(
+                    isHit = false,
+                    currentFrame = frames.toList(),
+                    evictedItem = victim
+                )
+            )
+        }
+    }
+    return steps
+}
+
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.myapplication
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,10 +12,29 @@ class ExampleUnitTest {
     @Test
     fun testFirstInFirstOut() {
         val result = firstInFirstOutAlgorithm(
-            listOf("1","2","1","2","3","4"),
+            listOf("1", "2", "1", "2", "3", "4"),
             3
         )
         println(result)
         assertEquals(4, 2 + 2)
     }
+
+    fun leastRecentlyUsed() {
+        val result = leastRecentlyUsedAlgorithm(
+            listOf("7", "0", "1", "2", "0", "3", "0", "4", "2", "3", "0", "3", "2"),
+            3
+        )
+        println(result)
+        assertEquals(4, 2 + 2)
+    }
+
+    fun optimal() {
+        val result = optimalAlgorithm(
+            listOf("7", "0", "1", "2", "0", "3", "0", "4", "2", "3"),
+            3
+        )
+        println(result)
+        assertEquals(4, 2 + 2)
+    }
+
 }
